97ee603254e5fdb1e758ae164cd89ba825063be1
RaphaelLuizPH <130625114+RaphaelLuizPH@users.noreply.github.com>
2024-06-10 10:09:42 -0300

    Added menu

---
+++ script.js
@@ -0,0 +1,28 @@
+AOS.init({duration: 400, delay: 400,}
+);
+
+function mudarTema() {
+  const html = document.documentElement;
+  html.classList.toggle("dark");
+}
+
+function startTime() {
+  const today = new Date();
+  let h = today.getHours();
+  let m = today.getMinutes();
+  let s = today.getSeconds();
+
+  m = ChecarTempo(m);
+  s = ChecarTempo(s);
+
+  document.getElementById("txt").innerHTML = h + ":" + m + ":" + s;
+
+  setTimeout(startTime, 1000);
+}
+
+function ChecarTempo(i) {
+  if (i < 10) {
+    i = "0" + i;
+  }
+  return i;
+}
